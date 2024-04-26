045c326907366288970d5c034d60b4ed45c2bee0
GoldenEagle1035 <goldeneagle1035@gmail.com>
2024-04-26 01:14:25 -0500

    collection info

--- src/data/collections.js
+++ src/data/collections.js
@@ -6,8 +6,6 @@ export const collections = [
         avatar: Av,
         address: "0xE6eBC5BE0f3Ed9f1fbE5a9CcE1CF20059AaFb6cc",
         interest: 120,
-        duration: 3600,
-        royalty: 12,
-        bestOffer: 0
+        duration: 3600
     }
 ]
